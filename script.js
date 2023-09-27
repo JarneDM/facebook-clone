ec9917e0015af53f5466cdb490e5db463dd09515
Jarne <jarne.demeyer05@hotmail.com>
2023-09-27 12:12:34 +0200

    fixing like buttons

--- script.js
+++ script.js
@@ -12,13 +12,28 @@ genderOptions.forEach(option => {
 });
 
 
-function toggleLike() {
-  let likeButton = document.getElementById('like-btn');
-  likeButton.classList.toggle('liked');
-}
 
-function toggleLikeTwo() {
-  let likeButton = document.getElementById('like-btn2');
-  likeButton.classList.toggle('liked');
-}
-
+// function toggleLikeTwo() {
+  //   let likeButton = document.getElementById('like-btn2');
+  //   likeButton.classList.toggle('liked');
+  // }
+  
+  function handleCount(likeButton) {
+    const counterId = likeButton.getAttribute('data-counter');
+    const numLikes = document.getElementById(counterId);
+    let count = parseInt(numLikes.textContent);
+  
+    if (likeButton.classList.contains('liked')) {
+      count++;
+    } else {
+      count--;
+    }
+  
+    numLikes.textContent = count;
+  }
+  
+  function toggleLike(likeButton) {
+    likeButton.classList.toggle('liked');
+    handleCount(likeButton);
+  }
+  
